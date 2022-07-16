a5b2a773a1420d6c59df24ec8830147416833511
Ahmad Abasi <a_abasi97@yahoo.com>
2022-07-16 20:33:43 +0430

    Making a new version

--- data/preprocess/preprocess.py
+++ data/preprocess/preprocess.py
@@ -4,7 +4,7 @@ from settings.settings import data_settings
 import pathlib
 from datetime import datetime
 
-
+"""
 def preprocess(data_name: str):
 
     csv_path = data_settings.csv_path
@@ -65,6 +65,57 @@ def preprocess(data_name: str):
         path + f"/train_{data_settings.tp_to_sl:02}.csv", index=False)
     test_df.to_csv(
         path + f"/test_{data_settings.tp_to_sl:02}.csv", index=False)
+
+"""
+
+
+def preprocess():
+
+    # Loading data and making it ready
+
+    df = pd.read_csv(
+        f"{data_settings.csv_path}/with_class/{data_settings.data_name}.csv")
+
+    train_starting_date = datetime(data_settings.train_split_year,
+                                   data_settings.train_split_month,
+                                   data_settings.train_split_day)
+
+    df["DateTime"] = pd.to_datetime(
+        df.pop("DateTime"), format=data_settings.date_time_format)
+    df = df.loc[df.DateTime > train_starting_date].reset_index(drop=True)
+
+    print("Number of rows with na values:", df.shape[0] - df.dropna().shape[0])
+    df = df.dropna().reset_index(drop=True)
+
+    date_time = df.pop("DateTime")
+    df.pop(df.windowsizes.name)
+    df.pop(df.do_nothing.name)
+
+    for column in data_settings.relative_columns:
+        if column in df.columns:
+            new_column = df[column].div(df.Close).mul(100)
+            new_column.name = f"{column}_C"
+            df = pd.concat([df, new_column], axis=1)
+
+    moving_averages = [column for column in data_settings.relative_columns if (
+        "SMA" in column) or ("EMA" in column)]
+
+    for column in moving_averages[:]:
+        moving_averages.remove(column)
+        for i in moving_averages[:]:
+            new_column = df[f"{column}"].div(df[f"{i}"])
+            new_column.name = f"{column}/{i}"
+            df = pd.concat([df, new_column], axis=1)
+
+    #TODO: Check
+    print("Number of rows with na values:", df.shape[0] - df.dropna().shape[0])
+    df.fillna(0, inplace=True)
+    df.replace(np.inf, 1e15, inplace=True)
+
+    df["DateTime"] = pd.to_datetime(
+        date_time, format=data_settings.date_time_format)
+
+    df.to_csv(data_settings.csv_path + f"/{data_name}.csv", index=False)
 
 
 if __name__ == "__main__":
